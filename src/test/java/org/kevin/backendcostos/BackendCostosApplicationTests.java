bc78212f7b6c6cb9185f6441d78360df75e62301
kevinjay4567 <kjayh@unal.edu.co>
2024-04-10 12:56:03 -0500

    test de ejemplo

--- src/test/java/org/kevin/backendcostos/BackendCostosApplicationTests.java
+++ src/test/java/org/kevin/backendcostos/BackendCostosApplicationTests.java
@@ -1,13 +1,31 @@
 package org.kevin.backendcostos;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class BackendCostosApplicationTests {
 
+    Calculator calculator = new Calculator();
+
     @Test
-    void contextLoads() {
+    void itShouldAddNumbers() {
+        // given
+        int n1 = 20;
+        int n2 = 30;
+
+        // when
+        int result = calculator.add(n1, n2);
+
+        // then
+        int expected = 50;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    static class Calculator {
+        int add(int a, int b) {
+            return a + b;
+        }
     }
 
 }
