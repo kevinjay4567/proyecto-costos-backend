b4f8e9620ab8b642d6271c8eb3d0130e8af44b65
kevinjay4567 <kjayh@unal.edu.co>
2024-04-10 12:57:26 -0500

    funcion de guardado de datos personas

--- src/main/java/org/kevin/backendcostos/services/datospersona/DatosPersonaServiceImpl.java
+++ src/main/java/org/kevin/backendcostos/services/datospersona/DatosPersonaServiceImpl.java
@@ -1,10 +1,11 @@
 package org.kevin.backendcostos.services.datospersona;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.kevin.backendcostos.entities.DatosPersonaEntity;
+import org.kevin.backendcostos.entities.*;
 import org.kevin.backendcostos.repositories.DatosPersonaRepository;
 import org.kevin.backendcostos.response.DatosPersonaResponse;
 import org.kevin.backendcostos.services.validation.ValidationServiceImpl;
@@ -44,6 +45,10 @@ public class DatosPersonaServiceImpl implements DatosPersonaService {
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DatosPersonaResponse("Datos invalidos", errors));
                 }
 
+                for (int i = 1; i < sheet.getLastRowNum(); i++) {
+                    save(sheet.getRow(i));
+                }
+
                 return ResponseEntity.ok(new DatosPersonaResponse("Archivo agregado correctamente"));
             } else {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -54,5 +59,24 @@ public class DatosPersonaServiceImpl implements DatosPersonaService {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(new DatosPersonaResponse("Error al subir el archivo"));
         }
+    }
+
+    private void save(Row row) {
+
+        // TODO: Pensar una mejor manera de hacer esto
+
+        String sede = row.getCell(0).getStringCellValue();
+        String vinculacion = row.getCell(1).getStringCellValue();
+        String actividad = row.getCell(2).getStringCellValue();
+        String facultad = row.getCell(3).getStringCellValue();
+        String programa = row.getCell(4).getStringCellValue();
+        float promedio = (float) row.getCell(5).getNumericCellValue();
+        String nivel = row.getCell(6).getStringCellValue();
+
+        DatosPersonaEntity datosPersonaEntity = DatosPersonaEntity.builder()
+                .sede(sede).vinculacion(vinculacion).actividad(actividad).facultad(facultad)
+                .programa(programa).promedio(promedio).nivel(nivel).build();
+
+        repository.save(datosPersonaEntity);
     }
 }
